b33f514c43cce57d1c7614247f2130e2e3b4cbac
kimError <alswjd7961@naver.com>
2021-04-22 16:45:04 +0900

    index.html 만듦

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask , render_template ,request ,redirect
+
+app = Flask(__name__)
+
+app.debug = True
+
+
+@app.route('/', methods=['GET'])
+def index():
+  # return "Hello World"
+  return render_template("index.html", data="KIM")
+
+
+
+if __name__ == '__main__':
+  app.run()
